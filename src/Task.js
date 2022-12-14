6503d64c4c7960df403e0cddb15e1a6df6b65506
RayhanHafidh <rayhanhafidh@gmail.com>
2022-12-14 23:59:17 +0700

    second commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "kosong" // TODO: replace this
+const url = "https://frontend-fp.web.app" // TODO: replace this
 const studentName = "Rayhan Hafidh WIarso" // TODO: replace this
 const studentId = "FE2717527" // TODO: replace this
 
